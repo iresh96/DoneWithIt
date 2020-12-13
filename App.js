5035f5d6184e8ecde11597211b8d3629ad13d480
Iresh Madushanka <ireshmadushanka03@gmail.com>
2020-12-13 15:04:52 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,24 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, SafeAreaView, Image, View } from "react-native";
+import ViewImageScreen from "./App/screens/ViewImageScreen";
+import WelcomeScreen from "./App/screens/WelcomeScreen";
 
 export default function App() {
-  let x = 1;
-  console.log("app executed");
-
-  return (
-    <View style={styles.container}>
-      <Text>You are awesome Iresh</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});

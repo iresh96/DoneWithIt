d4400abac0cbf0ae6452e4ef7022e8e44fd7c91e
Iresh Madushanka <ireshmadushanka03@gmail.com>
2020-12-27 10:44:37 +0530

    Image Library request

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import React, { useState } from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import { Button, Image } from "react-native";
 
-import LoggingScreen from "./app/screens/LoginScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
+import Screen from "./app/components/Screen";
+import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    console.log(granted);
+    if (!granted) alert("You need to enable permission to access library");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log("Image Picker Error", error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+      <ImageInput imageUri={imageUri} />
+    </Screen>
+  );
 }
